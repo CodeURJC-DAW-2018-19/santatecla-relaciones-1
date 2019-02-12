ab48aacfe31867f91bb9070c9cdc9c100bfd73b4
jorge g <jorgegb_13@hotmail.com>
2019-02-12 10:47:37 +0100

    Create User.java UserComponent.java
    Create the user and userComponent, tool.java and Unit.class it left implementation of Unit and Tool. In User  left the Autowired and the maps.

---
+++ backend/relaciones/src/main/java/es/santatecla/relaciones/user/User.java
@@ -0,0 +1,84 @@
+package es.santatecla.relaciones.user;
+
+
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String id;
+	
+	private String userName;
+	private String passwordHash;
+	
+	
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	private List<String> roles;
+	
+	
+	public User() {}
+	
+/*	public User(String userName, String passwordHash, List<String> roles) {
+		super();
+		this.userName= userName;
+		this.passwordHash = passwordHash;
+		this.roles = roles;
+			
+	}*/
+	
+	public User(String userName, String password, List<String> roles) {
+		this.userName = userName;
+		this.passwordHash = new BCryptPasswordEncoder().encode(password);
+		this.roles = roles;
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+	
+	
+	
+	
+
+}
---
+++ backend/relaciones/src/main/java/es/santatecla/relaciones/user/UserComponent.java
@@ -0,0 +1,34 @@
+package es.santatecla.relaciones.user;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+@Component
+	@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS )
+	public class UserComponent {
+	
+		private User user;
+		
+		private List<String> roles;
+		
+		public User getLoggedUser() {
+			return user;
+		}
+		
+		public void setLoggedUser(User user) {
+			this.user = user;
+		}
+		
+		public boolean isLoggedUser() {
+			return this.user != null;
+		}
+		
+		public boolean isUSerInRole(String str) {
+			return this.roles.contains(str);
+		}
+
+}
