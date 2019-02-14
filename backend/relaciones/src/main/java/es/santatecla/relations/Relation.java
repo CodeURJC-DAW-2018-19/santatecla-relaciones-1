73f7a95b1eb57185b2890a0a82cca66b9c45413e
mamonreal <ma.monreal@alumnos.urjc.es>
2019-02-14 11:53:23 +0100

    Add relations class.

--- backend/relaciones/src/main/java/es/santatecla/relations/Relation.java
+++ backend/relaciones/src/main/java/es/santatecla/relations/Relation.java
@@ -6,13 +6,17 @@ import java.util.List;
 import es.santatecla.enums.Relations;
 import es.santatecla.interfaces.IRelation;
 
-public class Relation implements IRelation {
+public class Relation<V> implements IRelation {
 	
-	private Dictionary<Relations, List<String>> relations;
+	private Dictionary<Relations, List<V>> relations;
 
-	public Relation (Dictionary<Relations, List<String>> relations) {
+	public Relation (Dictionary<Relations, List<V>> relations) {
 		this.relations = relations;
 	}
 	
-	
+	public void addRelation(Relations key, V value) {
+		List<V> values = this.relations.get(key);
+		values.add(value);
+		this.relations.put(key, values);
+	}
 }
