68af93ea04e94bbfd43b406694c18352db85064a
jorge g <jorgegb_13@hotmail.com>
2019-02-19 12:27:51 +0100

    Fixed some errors

--- backend/relaciones/src/main/java/es/santatecla/relaciones/controllers/UserController.java
+++ backend/relaciones/src/main/java/es/santatecla/relaciones/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import es.santatecla.relaciones.service.UserService;
 import es.santatecla.relaciones.user.User;
@@ -28,20 +29,31 @@ public class UserController extends UserService {
 		
 	}
 	
-	@RequestMapping("/user")//revisar
+	@RequestMapping("/user")//Request to show the user's name 
 	public String user(Model model) {
 		Boolean b = userComponent.isLoggedUser();
 		if (b) {
 			User user = userComponent.getLoggedUser();
-			user = uRepository.getById(user.getId());}
+			user = uRepository.getById(user.getId());
+			model.addAttribute(user.getName());
+		}
 			
 		return "/alumn-unit";		
-		
-			
-			
-			
 			
 		}
+	
+	@RequestMapping("addUser")
+	public String addUser(@RequestParam String name, @RequestParam String password) {
+		if (uRepository.findByName(name) == null){
+			User user = new User(name,password,"ROLE_USER");
+			uRepository.save(user);
+			userComponent.setLoggedUser(user);
+			return "/alumn-unit";
+		}
+		else {
+			return "error";//Custom error page
+		}
+	
 	}
 	
 	
