f26b478c415912c9cdc08f455cd6c9b196cbc06d
alilu8 <32311130+alilu8@users.noreply.github.com>
2019-02-24 17:22:07 +0100

    ErrorController
    ErrorController created and bug fixed

--- backend/relaciones/src/main/java/es/santatecla/relaciones/App.java
+++ backend/relaciones/src/main/java/es/santatecla/relaciones/App.java
@@ -11,6 +11,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
--- backend/relaciones/src/main/java/es/santatecla/services/RecordService.java
+++ backend/relaciones/src/main/java/es/santatecla/services/RecordService.java
@@ -4,7 +4,11 @@ import es.santatecla.models.Record;
 import es.santatecla.repositories.RecordRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RecordService
@@ -12,10 +16,17 @@ public class RecordService
     @Autowired
     private RecordRepository repository;
 
-    public void addEvent(Record event){
-        repository.save(event);
+    public void addRecord(Record record){
+        repository.save(record);
     }
-    public void deleteEventById(long id) {
+    public void deleteRecordById(long id) {
         repository.deleteById(id);
+    }
+    public Page<Record> findAll(Pageable page){
+        return repository.findAll(page);
+    }
+
+    public Optional<Record> findByOneId(Long id) {
+        return repository.findById(id);
     }
 }
