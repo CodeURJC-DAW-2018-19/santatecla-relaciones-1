57c4cef4d625091a574845e3ee11fd62e9f06e1e
alilu8 <32311130+alilu8@users.noreply.github.com>
2019-02-25 10:21:56 +0100

    Replace class

--- backend/relaciones/src/main/java/es/santatecla/relaciones/controllers/UploadController.java
+++ backend/relaciones/src/main/java/es/santatecla/relaciones/controllers/UploadController.java
@@ -6,7 +6,11 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import es.santatecla.relaciones.service.RecordService;
+import es.santatecla.relaciones.unit.Record;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,9 +18,13 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class UploadController {
+
+    @Autowired
+    RecordService recordService;
+
     private String filename;
     //Save the uploaded file to this folder
-    private static String UPLOADED_FOLDER = "C://Users/Angel/Documents/GitHub/santatecla-relaciones-1/backend/relaciones/src/main/resources/static/images/";
+    private static String UPLOADED_FOLDER = "src//main//resources//static//uploads//";
 
     /*
      **File upload page
@@ -28,7 +36,7 @@ public class UploadController {
     }
 
     @PostMapping("/upload")
-    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+    public String singleFileUpload(Model model, Record record, @RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
 
         if (file.isEmpty()) {
@@ -49,7 +57,7 @@ public class UploadController {
         catch (IOException e) {
             e.printStackTrace();
         }
-
+        recordService.addRecord(record);
         return "redirect:/uploadStatus";
     }
 
