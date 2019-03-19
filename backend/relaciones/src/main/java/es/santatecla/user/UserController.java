9fd3a934ae7578bf3b5294030fd761871dc09bcf
mamonreal <ma.monreal@alumnos.urjc.es>
2019-03-19 02:21:15 +0100

    h2 database

--- backend/relaciones/src/main/java/es/santatecla/user/UserController.java
+++ backend/relaciones/src/main/java/es/santatecla/user/UserController.java
@@ -4,41 +4,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController extends UserService {
-//	@Autowired
-//	private UserRepository userRepository;
-//	
-//	@Autowired
-//	private UserComponent userComponent;
-//	
-//	@Autowired
-//	private UnitRepository unitRepository;
 	
-	
-//	@PostConstruct
-//	public void init() {
-//		userRepository.save(new User("Miguel","1234", "ROLE_ADMIN","ROLE_USER"));
-//		userRepository.save(new User("Carlos","pass", "ROLE_USER"));
-//		
-//	}
-	
-//	@GetMapping("/")
-//	public String showUnits(Model model) {
-//		model.addAttribute("unit",unitRepository.findAll());
-//		return "/index";
-//	}
-	
-//	@RequestMapping("/user")//Request to show the user's name 
-//	public String user(Model model) {
-//		Boolean b = userComponent.isLoggedUser();
-//		model.addAttribute("logged",b);
-//		if (b) {
-//			User user = userComponent.getLoggedUser();
-//			user = userRepository.getById(user.getId());
-//			model.addAttribute(user.getName());
-//		}
-//			
-//		return "/index";//Have to create	user template to show the user	
-			
 }
 	
 	
