cb6e34998a9ca9a1d7dda14906e92da6a7841af1
mamonreal <ma.monreal@alumnos.urjc.es>
2019-03-25 13:02:28 +0100

    fix record

--- backend/relaciones/src/main/java/es/santatecla/record/Record.java
+++ backend/relaciones/src/main/java/es/santatecla/record/Record.java
@@ -1,71 +1,70 @@
-//package es.santatecla.record;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.ManyToOne;
-//
-//import org.springframework.data.annotation.Id;
-//
-//import es.santatecla.enums.RecordsEnum;
-//import es.santatecla.unit.Unit;
-//
-//@Entity
-//public class Record<V> {
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.AUTO)
-//	private long id;
-//	
-//	@ManyToOne
-//	private Unit unit;
-//	
-//	private RecordsEnum key;
-//	
-//	private V value;
-//	
-//	private String image;
-//
-//	public Record() {
-//		
-//	}
-//	
-//	public Record(Unit unit, RecordsEnum key, V value, String image) {
-//		super();
-//		this.unit = unit;
-//		this.key = key;
-//		this.value = value;
-//		this.image = image;
-//	}
-//
-//	public Unit getUnit() {
-//		return unit;
-//	}
-//
-//	public void setUnit(Unit unit) {
-//		this.unit = unit;
-//	}
-//
-//	public RecordsEnum getKey() {
-//		return key;
-//	}
-//
-//	public void setKey(RecordsEnum key) {
-//		this.key = key;
-//	}
-//
-//	public V getValue() {
-//		return value;
-//	}
-//
-//	public void setValue(V value) {
-//		this.value = value;
-//	}
-//
-//	public String getImage() {
-//		return image;
-//	}
-//
-//	public void setImage(String image) {
-//		this.image = image;
-//	}
-//}
+package es.santatecla.record;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import es.santatecla.enums.RecordsEnum;
+import es.santatecla.unit.Unit;
+
+@Entity
+public class Record {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@ManyToOne
+	private Unit unit;
+	
+	private RecordsEnum key;
+	
+	private String value;
+	
+	private String image;
+
+	public Record() {
+		
+	}
+	
+	public Record(Unit unit, RecordsEnum key, String value, String image) {
+		super();
+		this.unit = unit;
+		this.key = key;
+		this.value = value;
+		this.image = image;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Unit getUnit() {
+		return unit;
+	}
+
+	public void setUnit(Unit unit) {
+		this.unit = unit;
+	}
+
+	public RecordsEnum getKey() {
+		return key;
+	}
+
+	public void setKey(RecordsEnum key) {
+		this.key = key;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+}
