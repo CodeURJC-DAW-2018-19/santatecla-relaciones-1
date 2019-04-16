a6dc25a5725e4cc6ef19ad3ea898d7809ad8f200
Miguel <ma.monreal@alumnos.urjc.es>
2019-04-17 00:35:24 +0200

    Record service

--- backend/relaciones/src/main/java/es/santatecla/record/RecordService.java
+++ backend/relaciones/src/main/java/es/santatecla/record/RecordService.java
@@ -3,23 +3,37 @@ package es.santatecla.record;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import es.santatecla.enums.RecordsEnum;
+import es.santatecla.unit.Unit;
+
 @Service
 public class RecordService
 {
-//    @Autowired
-//    private RecordRepository recordRepository;
-//
-//    public void addRecord(Record record){
-//        repository.save(record);
-//    }
-//    public void deleteRecordById(long id) {
-//        repository.deleteById(id);
-//    }
-//    public Page<Record> findAll(Pageable page){
-//        return repository.findAll(page);
-//    }
-//
-//    public Optional<Record> findByOneId(Long id) {
-//        return repository.findById(id);
-//    }
+    private RecordRepository recordRepository;
+	
+	@Autowired
+	public RecordService(RecordRepository recordRepository) {
+    	this.recordRepository = recordRepository;
+    }
+	
+	public Record addRecord(Unit unit, RecordsEnum type, String value) {
+		Record record = new Record(unit, type, value, null);
+		return this.recordRepository.save(record);
+	}
+
+	public Record addImage(Unit unit, RecordsEnum type, String image) {
+		Record record = new Record(unit, type, null, image);
+		return this.recordRepository.save(record);
+	}
+	
+	/**
+	 * Delete a record (or image) from the database.
+	 * @param recordId
+	 */
+	public void deleteRecord(long recordId) {
+		Record record = this.recordRepository.findById(recordId);
+		if (record != null) {
+			this.recordRepository.delete(record);
+		}
+	}
 }
