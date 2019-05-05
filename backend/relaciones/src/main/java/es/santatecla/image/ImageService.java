e712410293c748003a699dc263ab1e9260d7c2ab
Miguel <ma.monreal@alumnos.urjc.es>
2019-05-05 20:58:46 +0200

    ImageService

--- backend/relaciones/src/main/java/es/santatecla/image/ImageService.java
+++ backend/relaciones/src/main/java/es/santatecla/image/ImageService.java
@@ -1,8 +1,69 @@
 package es.santatecla.image;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ImageService {
 
+	private AtomicInteger imageId = new AtomicInteger();
+	private Map<Integer, Image> images = new ConcurrentHashMap<>();
+	private static final Path FILES_FOLDER = Paths.get(System.getProperty("user.dir"), "images");
+	
+	public String uploadPhoto(Model model, String imageTitle, MultipartFile file) {
+		int id = imageId.getAndIncrement();
+
+        String fileName = "image-" + id + ".jpg";
+
+        if (!file.isEmpty()) {
+            try {
+
+                File uploadedFile = new File(FILES_FOLDER.toFile(), fileName);
+                file.transferTo(uploadedFile);
+
+                images.put(id, new Image(id, imageTitle));
+
+                return "upload-status";
+
+            } catch (Exception e) {
+
+                model.addAttribute("error", e.getClass().getName() + ":" + e.getMessage());
+
+                return "uploaded";
+            }
+        } else {
+
+            model.addAttribute("error", "The file is empty");
+
+            return "upload-status";
+        }
+	}
+	
+	public void getPhoto(String id, HttpServletResponse res) throws IOException {
+		String fileName = "image-" + id + ".jpg";
+
+        Path image = FILES_FOLDER.resolve(fileName);
+
+        if (Files.exists(image)) {
+            res.setContentType("image/jpeg");
+            res.setContentLength((int) image.toFile().length());
+            FileCopyUtils.copy(Files.newInputStream(image), res.getOutputStream());
+
+        } else {
+            res.sendError(404, "File" + fileName + "(" + image.toAbsolutePath() + ") does not exist");
+        }
+	}
 }
--- backend/relaciones/src/main/java/es/santatecla/image/UploadController.java
+++ backend/relaciones/src/main/java/es/santatecla/image/UploadController.java
@@ -1,100 +1,47 @@
 package es.santatecla.image;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
 @Controller
 public class UploadController {
 
-//    @Autowired
-//    RecordService recordService;
-
-    private static final Path FILES_FOLDER = Paths.get(System.getProperty("user.dir"), "images");
-
-    private AtomicInteger imageId = new AtomicInteger();
-    private Map<Integer, Image> images = new ConcurrentHashMap<>();
-
-    @PostConstruct
-    public void init() throws IOException {
-
-        if (!Files.exists(FILES_FOLDER)) {
-            Files.createDirectories(FILES_FOLDER);
-        }
-    }
-
-    @RequestMapping("/image-upload")
-    public String index(Model model) {
-
-        model.addAttribute("images", images.values());
-
-        return "image-upload";
-    }
-
+	private ImageService imageService;
+	
+	@Autowired
+	public UploadController(ImageService imageService) {
+		this.imageService = imageService;
+	}
+	
+//	@RequestMapping("/image-upload")
+//    public String index(Model model) {
+//
+//        model.addAttribute("images", images.values());
+//
+//        return "image-upload";
+//    }
+//
     @RequestMapping(value = "/image/upload", method = RequestMethod.POST)
     public String handleFileUpload(Model model, @RequestParam(value ="imageTitle") String imageTitle,
                                    @RequestParam(value ="file") MultipartFile file) {
 
-        int id = imageId.getAndIncrement();
-
-        String fileName = "image-" + id + ".jpg";
-
-        if (!file.isEmpty()) {
-            try {
-
-                File uploadedFile = new File(FILES_FOLDER.toFile(), fileName);
-                file.transferTo(uploadedFile);
-
-                images.put(id, new Image(id, imageTitle));
-
-                return "upload-status";
-
-            } catch (Exception e) {
-
-                model.addAttribute("error", e.getClass().getName() + ":" + e.getMessage());
-
-                return "uploaded";
-            }
-        } else {
-
-            model.addAttribute("error", "The file is empty");
-
-            return "upload-status";
-        }
+        return this.imageService.uploadPhoto(model, imageTitle, file);
     }
 
-    @RequestMapping("/image/{id}")
+	@RequestMapping("/image/{id}")
     public void handleFileDownload(@PathVariable String id, HttpServletResponse res)
             throws FileNotFoundException, IOException {
 
-        String fileName = "image-" + id + ".jpg";
-
-        Path image = FILES_FOLDER.resolve(fileName);
-
-        if (Files.exists(image)) {
-            res.setContentType("image/jpeg");
-            res.setContentLength((int) image.toFile().length());
-            FileCopyUtils.copy(Files.newInputStream(image), res.getOutputStream());
-
-        } else {
-            res.sendError(404, "File" + fileName + "(" + image.toAbsolutePath() + ") does not exist");
-        }
+        this.imageService.getPhoto(id, res);
     }
 }
