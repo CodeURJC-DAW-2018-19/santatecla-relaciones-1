c45fc635c2981d6906ec9a25cffc63f47b9f2b0b
Miguel <ma.monreal@alumnos.urjc.es>
2019-05-24 23:39:25 +0200

    add child not fixed yet

--- backend/relaciones/src/main/java/es/santatecla/security/LoginController.java
+++ backend/relaciones/src/main/java/es/santatecla/security/LoginController.java
@@ -18,51 +18,11 @@ import es.santatecla.user.User;
 import es.santatecla.user.UserComponent;
 
 
-//@RestController
-//public class LoginController {
-//
-//	private static final Logger log = LoggerFactory.getLogger(LoginController.class);
-//
-//	@Autowired
-//	private UserComponent userComponent;
-//
-//	@RequestMapping("/api/logIn")
-//	public ResponseEntity<User> logIn() {
-//		
-//		if (!userComponent.isLoggedUser()) {
-//			log.info("Not user logged");
-//			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//		} else {
-//			User loggedUser = userComponent.getLoggedUser();
-//			log.info("Logged as " + loggedUser.getName());
-//			return new ResponseEntity<>(loggedUser, HttpStatus.OK);
-//		}
-//	}
-//
-//	@RequestMapping("/api/logOut")
-//	public ResponseEntity<Boolean> logOut(HttpSession session) {
-//
-//		if (!userComponent.isLoggedUser()) {
-//			log.info("No user logged");
-//			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//		} else {
-//			session.invalidate();
-//			log.info("Logged out");
-//			return new ResponseEntity<>(true, HttpStatus.OK);
-//		}
-//	}
-//
-//}
 @Controller
 public class LoginController {
 	
 	@Autowired
 	UserComponent userComponent;
-	
-//	@RequestMapping("/")
-//	public String home() {
-//		return"/";
-//	}
 	
 	@RequestMapping("/login")
 	public String login(Model model, HttpServletRequest request) {
@@ -79,18 +39,4 @@ public class LoginController {
 		model.addAttribute("loginerror",true);
 		return "/loginerror";//must see loginerror.html
 	}
-	
-//	@RequestMapping("/admin")
-//	public String admin() {
-//		return "teacher-units";
-//	}
-//	
-//	@RequestMapping("/showUser")
-//	public String show(Model model, HttpServletRequest request) {
-//		model.addAttribute("logged",userComponent.isLoggedUser());
-//			if (userComponent.isLoggedUser()){
-//				model.addAttribute("name", userComponent.getLoggedUser().getName());
-//			}
-//		return "index";
-//			}
 }
