285370ce5c3535e6c6949a9beb511db646dfc641
Miguel <ma.monreal@alumnos.urjc.es>
2019-05-24 23:50:28 +0200

    add unit fixed

--- backend/relaciones/src/main/java/es/santatecla/unit/UnitController.java
+++ backend/relaciones/src/main/java/es/santatecla/unit/UnitController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.santatecla.enums.RelationsEnum;
+import es.santatecla.relation.RelationService;
 import es.santatecla.user.UserComponent;
 
 
@@ -27,6 +28,9 @@ public class UnitController {
 	
 	@Autowired
 	private UserComponent userComponent;
+
+	@Autowired
+	private RelationService relationService;
 	
 	@ModelAttribute
 	public void addUserToModel(Model model) {
@@ -69,6 +73,14 @@ public class UnitController {
 		model.addAttribute("part", parts);
 		
 		return "/alumn-units";
+	}
+
+	@RequestMapping("/add-child")
+	public String addChildFromUnit(Model model, @RequestParam String id, @RequestParam String relatedId){
+		int unitId = Integer.parseInt(id);
+		int relatedUnitId = Integer.parseInt(relatedId);
+		this.relationService.AddRelations(unitId, relatedUnitId, RelationsEnum.CHILD );
+	return this.getUnit(model, unitId);
 	}
 
 	
