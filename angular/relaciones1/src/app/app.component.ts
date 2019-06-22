2ff563f7fb2be3340191331e6b9142ce82fcde06
Miguel <ma.monreal@alumnos.urjc.es>
2019-06-22 19:14:10 +0200

    add matirial dashboard styles

--- angular/relaciones1/src/app/app.component.ts
+++ angular/relaciones1/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss',
+    '../../node_modules/material-dashboard/assets/scss/material-dashboard.scss'
+  ]
 })
 export class AppComponent {
   title = 'relaciones1';
