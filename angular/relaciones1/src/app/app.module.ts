95312ca4f330b86a08dfe47ee7bb14d13515d320
Miguel <ma.monreal@alumnos.urjc.es>
2019-06-23 22:26:56 +0200

    relation component

--- angular/relaciones1/src/app/app.module.ts
+++ angular/relaciones1/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RelationComponent } from './relation/relation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RelationComponent
   ],
   imports: [
     BrowserModule,
