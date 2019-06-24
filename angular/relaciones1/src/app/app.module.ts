b6ef8b6e61de730dc63b201bcb3e077857448589
Miguel <ma.monreal@alumnos.urjc.es>
2019-06-24 21:20:09 +0200

    Basic angular components

--- angular/relaciones1/src/app/app.module.ts
+++ angular/relaciones1/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RelationComponent } from './relation/relation.component';
+import { RecordComponent } from './record/record.component';
+import { RelationsComponent } from './relations/relations.component';
+import { RecordsComponent } from './records/records.component';
+import { UnitComponent } from './unit/unit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RelationComponent
+    RelationComponent,
+    RecordComponent,
+    RelationsComponent,
+    RecordsComponent,
+    UnitComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/relaciones1/src/app/relations/relations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-relations',
+  templateUrl: './relations.component.html',
+  styleUrls: ['./relations.component.scss']
+})
+export class RelationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
