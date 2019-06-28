e6d378dba0335e129f85cdd12554b7f7e51ea0de
theyorch11 <jorgegb_13@hotmail.com>
2019-06-28 17:40:06 +0200

    Add routes for login

--- angular/relaciones1/src/app/record/record.component.ts
+++ angular/relaciones1/src/app/record/record.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-record',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordComponent implements OnInit {
 
+
+  @Input() unitId:number;
+  @Input() type:string;
+  @Input() value:string;
+  
   constructor() { }
 
   ngOnInit() {
